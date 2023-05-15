f1bc62d34077a5e7ab022378e69c75c8bb1b8f7c
sumeetwd25 <131378640+sumeetwd25@users.noreply.github.com>
2023-05-15 20:01:35 +0530

    Add files via upload

---
+++ ReactApps/ietmay23/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Welcome from './Welcome';
+import Greeting from './Greeting';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello From React</h1>
+      <Welcome></Welcome>
+      <Greeting></Greeting>
+    </div>
+  );
+}
+
+export default App;
---
+++ ReactApps/ietmay23/src/Greeting.js
@@ -0,0 +1,3 @@
+export default function Greeting(){
+    return <div>Greeting from function component</div>
+}
