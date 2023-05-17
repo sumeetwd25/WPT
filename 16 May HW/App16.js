a7a9eb662aa8c7ebaeea0a690586cf4da0aa8cf2
sumeetwd25 <131378640+sumeetwd25@users.noreply.github.com>
2023-05-17 11:03:43 +0530

    Add files via upload

---
+++ 16 May HW/App16.js
@@ -0,0 +1,31 @@
+// import Add from "./Add"
+// import Factorial from "./Factorial"
+// import TrialProps from "./TrialProps"
+// import Que3 from "./Que3"
+import Que4 from "./Que4"
+
+
+export default function App16(){
+
+    // let obj={xcoordinate:12, ycoordinate:20}
+
+    // function alpha(){
+    //     console.log("Alpha called")
+    // }
+
+    return <div>
+        {/* <Add num1={12} num2={13}></Add>
+        <Add num1={100} num2={13}></Add>
+        <Add num1={121} num2={13}></Add>
+        <Add num1={1} num2={3}></Add>
+
+        <Factorial num1={5}></Factorial>
+
+        <TrialProps func1={alpha} flag={true} point={obj} num1={45} str={"PPP"} arr={[10,20,30]}></TrialProps>
+
+        <Que3></Que3> */}
+
+        <Que4></Que4>
+
+    </div>
+}
